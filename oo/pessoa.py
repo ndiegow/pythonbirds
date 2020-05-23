f3c91badf21fa1cdde94367c38c934b3156da2cf
diego oliveira <dospawx@MacBook-Air-de-diego.local>
2020-05-23 15:45:25 -0300

    adicionando alterações

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,3 +1,16 @@
 class Pessoa:
-    pass
+
+    def __init__(self, nome = None, idade = None):
+        self.nome = nome
+        self.idade = idade
+
+
+    def cumprimentar(self):
+        print("olá")
+
+if __name__ == "__main__":
+    p = Pessoa("Diego")
+    p.cumprimentar()
+    print(p.nome)
+
 
