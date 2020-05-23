23c785d8c0b64fd10574052d2070f8d17784c1c7
diego oliveira <dospawx@MacBook-Air-de-diego.local>
2020-05-23 16:10:26 -0300

    objetos complexos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,23 @@
 class Pessoa:
 
-    def __init__(self, nome = None, idade = None):
+    def __init__(self, *filhos, nome = None, idade = None):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
 
     def cumprimentar(self):
         print("olá")
 
 if __name__ == "__main__":
-    p = Pessoa("Diego")
-    p.cumprimentar()
-    print(p.nome)
+    print("oi")
+    renzo = Pessoa(nome = "Renzo")
+    lucas = Pessoa(nome = "Lucas")
+    luciano = Pessoa(renzo,lucas,nome = "Luciano")
+
+    for filho in luciano.filhos:
+        print(filho.nome)
+
+
 
 
