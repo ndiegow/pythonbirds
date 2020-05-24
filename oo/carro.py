2452916ffe968b1b690360dd4bdc9fbf880aa201
diego oliveira <dospawx@MacBook-Air-de-diego.local>
2020-05-23 23:24:57 -0300

    metodos de classe

---
+++ oo/carro.py
@@ -0,0 +1,64 @@
+class Motor:
+    def __init__(self):
+        self.velocidade = 0
+    
+    def acelerar(self):
+        self.velocidade += 1
+
+    def frear(self):
+        self.velocidade-=1
+
+
+class Direcao:
+    
+    def __init__(self):
+        self.direcao = 'N'
+
+    def girar_a_esquerda(self):
+        if self.direcao == 'N': self.direcao = 'O'
+        elif self.direcao == 'O': self.direcao = 'S'
+        elif self.direcao == 'S': self.direcao = 'L'
+        elif self.direcao == 'L': self.direcao = 'N'
+        
+    def girar_a_direita(self):
+        if self.direcao == 'N': self.direcao = 'L'
+        elif self.direcao == 'L': self.direcao = 'S'
+        elif self.direcao == 'S': self.direcao = 'O'
+        elif self.direcao == 'O': self.direcao = 'N'
+
+class Carro:
+
+    def __init__(self,direcao, motor):
+        self.direcao = direcao
+        self.motor = motor
+    
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+    
+    def frear(self):
+        self.motor.frear()
+
+    def girar_a_esquerda(self):
+        return self.direcao.girar_a_esquerda()
+
+    def girar_a_direita(self):
+        return self.direcao.girar_a_direita()
+
+
+
+if __name__ == "__main__":
+    V8 = Motor()
+    dir = Direcao()
+    ferrari = Carro(dir,V8)
+    ferrari.acelerar()
+    print(ferrari.calcular_velocidade())
+    ferrari.acelerar()
+    print(ferrari.calcular_velocidade())
+    
+
+
+
+
--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,7 @@
 class Pessoa:
+    olhos = 2
+    pernas = 2
+    bracos = 2
 
     def __init__(self, *filhos, nome = None, idade = None):
         self.nome = nome
@@ -9,14 +12,41 @@ class Pessoa:
     def cumprimentar(self):
         print("olá")
 
+    def _acenar():
+        pass
+
+    @staticmethod
+    def metodo_estatico(a,b):
+        return a+b
+    
+    @classmethod
+    def metodo_de_classe(cls):
+        return cls.olhos + cls.pernas 
+
+
 if __name__ == "__main__":
     print("oi")
     renzo = Pessoa(nome = "Renzo")
     lucas = Pessoa(nome = "Lucas")
     luciano = Pessoa(renzo,lucas,nome = "Luciano")
+    print(luciano.metodo_estatico(1,2))
+    print(luciano.metodo_de_classe())
+    print(Pessoa.metodo_de_classe())
+
 
     for filho in luciano.filhos:
         print(filho.nome)
+        print(filho.olhos)
+
+
+class Opcao:
+    def __init__(self):
+        self.premio = premio
+        self.vencimento = vencimento
+        self.ativo = None
+        self.vencimento = None
+
+
 
 
 
